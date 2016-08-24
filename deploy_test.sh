a4923dc12cbae1e090ac5fa1fe9ddd6c83dec70a
Quentin Nerden <quentin.nerden@gmail.com>
2016-08-24 19:41:29 +0200

    Check all exercices have a test and a good example

---
+++ deploy_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e # exit if any errors
+
+for dir in exercises/*  ; do
+
+  ex_name="${dir/exercises\//}" # keep only dir name, not path
+  echo "### Checking all tests pass for exercise '${ex_name}'."
+
+  cd "${dir}" || echo "Could not 'cd ${dir}'!"
+
+  # some exercices have directories and exercices name that differ: '-' vs '_'
+  if [ ! -e "${ex_name}_test.sh" ]; then
+    ex_name2="${ex_name/-/_}"
+    if [ ! -e "${ex_name2}_test.sh" ]; then
+      echo "Files ${dir}/${ex_name}_test.sh or ${dir}/${ex_name2}_test.sh do not exit. Aborting."
+      exit 1
+    fi
+    ex_name="${ex_name2}"
+  fi
+
+  # create the exercice file that the test expect
+  cp example.sh "${ex_name}.sh"
+
+  # check if the tests pass for this exercise
+  bats "${ex_name}_test.sh"
+
+
+  cd ../../
+
+  echo
+done
